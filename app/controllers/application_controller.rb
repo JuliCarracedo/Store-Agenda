0e83caa549bae7b2b183664b14722be6a3d9ec39
JuliCarracedo <julicarra@hotmail.com>
2022-02-26 22:51:04 -0300

    Create json response generator as helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+# json_response is a helper method in charnge of creating and rendering responses
+
+    def json_response object, status = 200
+        render json: object, status: status
+    end
+
+    def json_error error , status
+        render json: error , status: status
+    end
+
 end
