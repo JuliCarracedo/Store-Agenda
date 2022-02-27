9871e3532f2ae0d91b24a00c07e46b7245518ab5
JuliCarracedo <julicarra@hotmail.com>
2022-02-26 22:50:10 -0300

    Add validations to Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+    validates :code, presence: true
+    validates :name, presence: true, length: {maximum: 25}
+    validates :price, numericality: {greater_than: 0}
 end
