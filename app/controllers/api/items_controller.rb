0f6be97dc3d7a9401ba7e22556806d332a49e9ee
JuliCarracedo <julicarra@hotmail.com>
2022-02-26 23:36:33 -0300

    Create endpoint for updating prices

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -4,12 +4,45 @@ class Api::ItemsController < ApplicationController
     def index 
         @items = Item.all
         @list = @items.map do |item| 
-            {id: item.id
+            {id: item.id,
              code: item.code, 
              name: item.name, 
-             price: "€#{sprintf('%.2f',item.price.to_f / 100)}"}
+             price: num_to_price(item.price)}
         end
         json_response(@list)
+    end
+
+    #Updates item price if the parameters are correctly parsed
+    #Parameters: {id: Integer, price: Positive Integer}
+    def update
+
+        if params[:price].match(/[€.]/)
+            json_error('Price must be expressed in cents and without symbols', 422) 
+            return
+        end
+
+        if params[:price].to_f <= 0
+            json_error('Price must be a positive number', 422)
+            return 
+        end 
+
+        @item = Item.find(params[:id])
+        if @item
+           if @item.update(price: params[:price])
+            price = num_to_price(params[:price])
+            json_response("#{@item.code} updated to #{ price }")
+           else 
+            json_error('Something went wrong', 402)
+           end
+        else
+            json_error('Item does not exist', 422)
+        end
+    end
+
+    private
+
+    def num_to_price i
+        "€#{sprintf('%.2f',i.to_f / 100)}"
     end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 # json_response is a helper method in charnge of creating and rendering responses
+    skip_before_action :verify_authenticity_token
 
     def json_response object, status = 200
         render json: object, status: status
